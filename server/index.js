260c61097f7946cde0928e17fa8a6dbe02c9f2ed
Peter Higgins <pah@cenabo.com>
2018-03-19 14:19:24 +0000

    Add authentication with Google OAuth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStretegy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+
+passport.use(new GoogleStretegy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
