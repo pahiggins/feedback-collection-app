46f9bf670c2dc26d1b920193c5b8bcdf2257b99d
Peter Higgins <pah@cenabo.com>
2018-03-26 09:47:22 +0100

    Add create survey functionality

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Dashboard = () => <div>Dashboard</div>;
+const Dashboard = () => {
+  return (
+    <div>
+      <h3>Dashboard</h3>
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
---
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SurveyFormReview extends Component {
+  render() {
+    return (
+      <div>
+        <h3>SurveyFormReview</h3>
+        
+      </div>
+    );
+  }
+};
+
+export default SurveyFormReview;
